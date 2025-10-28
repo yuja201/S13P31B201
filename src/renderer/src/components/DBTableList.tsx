e0727f91817cd2dea7aad74fa613a85f771ab93a
wlscksdb <wlscksdb@naver.com>
2025-10-28 16:17:25 +0900

    style: Adjust MiniSearchBox width

--- src/renderer/src/components/DBTableList.tsx
+++ src/renderer/src/components/DBTableList.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { FaSearch } from 'react-icons/fa'
-
 import { TableInfo } from '@renderer/views/CreateDummyView'
+import MiniSearchBox from '@renderer/components/MiniSearchBox'
 
 type DBTableListProps = {
   tables: TableInfo[]
@@ -12,40 +11,40 @@ type DBTableListProps = {
 const DBTableList: React.FC<DBTableListProps> = ({ tables, focusedTableId, onTableSelect }) => {
   return (
     <>
-      {/* --- 테이블 목록 헤더 --- */}
-      <div className="list-header">
-        <h3 className="preSemiBold18">테이블 목록</h3>
-        <span className="preRegular14">{tables.length} table</span>
+      <div className="table-list-container">
+        {/* --- 테이블 목록 헤더 --- */}
+        <div className="list-header">
+          <h3 className="preSemiBold20">테이블 목록</h3>
+          <span className="preRegular14">{tables.length} table</span>
+        </div>
+
+        <div className="search-bar">
+          <MiniSearchBox />
+        </div>
+
+        {/* --- 테이블 리스트 --- */}
+        <ul className="table-list">
+          {tables.map((table) => (
+            <li
+              key={table.id}
+              // 현재 포커스된 테이블인지 확인하고 'active' 클래스 부여
+              className={`table-list-item ${table.id === focusedTableId ? 'active' : ''}`}
+              onClick={() => onTableSelect(table)} // 클릭 시 부모의 상태 변경 함수 호출
+            >
+              <input
+                type="checkbox"
+                defaultChecked={table.id === 'users' || table.id === 'categories'}
+              />
+              <div className="table-item-info">
+                <span className="preMedium16">{table.name}</span>
+                <span className="preRegular12">
+                  {table.columns} columns · {table.rows} row
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className="search-bar">
-        <FaSearch color="var(--color-placeholder)" />
-        <input type="text" placeholder="테이블 검색" className="preRegular14" />
-      </div>
-
-      {/* --- 테이블 리스트 --- */}
-      <ul className="table-list">
-        {tables.map((table) => (
-          <li
-            key={table.id}
-            // 현재 포커스된 테이블인지 확인하고 'active' 클래스 부여
-            className={`table-list-item ${table.id === focusedTableId ? 'active' : ''}`}
-            onClick={() => onTableSelect(table)} // 클릭 시 부모의 상태 변경 함수 호출
-          >
-            <input
-              type="checkbox"
-              defaultChecked={table.id === 'users' || table.id === 'categories'}
-            />
-            <div className="table-item-info">
-              <span className="preMedium16">{table.name}</span>
-              <span className="preRegular12">
-                {table.columns} columns · {table.rows} row
-              </span>
-            </div>
-          </li>
-        ))}
-      </ul>
-
       {/* --- 하단 선택 개수 --- */}
       <div className="list-footer preRegular14">
         <span>선택된 테이블</span>
@@ -54,30 +53,30 @@ const DBTableList: React.FC<DBTableListProps> = ({ tables, focusedTableId, onTab
 
       {/* --- TableList 전용 스타일 --- */}
       <style>{`
+        .table-list-container{
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
         .list-header {
+          width: 100%;
           display: flex;
           flex-direction: column;
           justify-content: space-between;
-          align-items: baseline;
+          align-items: flex-start;
           margin-bottom: 16px;
+          gap: 4px;
         }
         .list-header span {
           color: var(--color-dark-gray);
+          padding: 4px;
         }
         .search-bar {
+          width: 100%;
           display: flex;
           align-items: center;
-          gap: 8px;
-          background-color: var(--color-background);
           border-radius: 6px;
-          padding: 8px 12px;
           margin-bottom: 16px;
-        }
-        .search-bar input {
-          border: none;
-          background: transparent;
-          width: 100%;
-          outline: none;
         }
         .table-list {
           list-style: none;
