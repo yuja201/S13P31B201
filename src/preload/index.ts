ba9ff715e62b3c53a50b2b665c17849240ca49a6
wlscksdb <wlscksdb@naver.com>
2025-11-10 13:23:22 +0900

    fix: change any type to unknown

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -104,14 +104,14 @@ const api = {
       databaseId: number
       table: string
       column: string
-    }): Promise<{ sample: any }> => ipcRenderer.invoke('db:get-random-sample', params),
+    }): Promise<{ sample: unknown }> => ipcRenderer.invoke('db:get-random-sample', params),
 
     // FK Value Verification Function
     validateFkValue: (params: {
       databaseId: number
       table: string
       column: string
-      value: any
+      value: unknown
     }): Promise<{ isValid: boolean }> => ipcRenderer.invoke('db:validate-fk-value', params)
   },
 
