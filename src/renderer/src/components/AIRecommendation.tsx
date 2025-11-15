65eb0336f20e1aae98efcb3d77b1242ca987c3fb
hynseoj <wjdgustj2877@naver.com>
2025-11-16 03:50:53 +0900

    fix: Build error

--- src/renderer/src/components/AIRecommendation.tsx
+++ src/renderer/src/components/AIRecommendation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import AIRecommendationCard from './AIRecommendationCard'
 import UserQueryAIModal from '@renderer/modals/UserQueryAIModal'
 import LoadingSpinner from '@renderer/components/LoadingSpinner'
